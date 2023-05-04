d7d47d197bfd4922e720a1ec2e49d4fc1f5f2ebe
Clint <clintpurser@gmail.com>
2023-05-04 10:46:26 -0400

    remove args and returns from documentation

--- lib/src/components/servo/servo.dart
+++ lib/src/components/servo/servo.dart
@@ -16,24 +16,15 @@ abstract class Servo extends Resource {
   Servo(this.name);
 
   /// Move the [Servo] to the provided angle.
-  ///
-  /// Args:
-  ///     [angle] (int): The desired angle of the servo in degrees.
   Future<void> move(int angle, {Map<String, dynamic>? extra});
 
   /// Get the current angle (degrees) of the [Servo].
-  ///
-  /// Returns:
-  ///     int: The current angle of the servo in degrees.
   Future<int> getPosition({Map<String, dynamic>? extra});
 
   /// Stop the [Servo]. It is assumed that the servo stops immediately.
   Future<void> stop({Map<String, dynamic>? extra});
 
   /// Get if the [Servo] is currently moving.
-  ///
-  /// Returns:
-  ///     bool: Whether the servo is moving.
   Future<bool> isMoving();
 
   static ResourceName getResourceName(String name) {
