1b22e0f33538ef6988835d24695f2bffe6cd004e
Clint <clint.purser@viam.com>
2023-05-08 11:26:03 -0400

    RSDK-2828 - add documentation to motor.dart (#10)
    add documentation to motor.dart

--- lib/src/components/motor/motor.dart
+++ lib/src/components/motor/motor.dart
@@ -25,6 +25,10 @@ class PowerState {
   }
 }
 
+/// Motor represents a physical motor.
+///
+/// This acts as an abstract base class for any drivers representing specific motor implementations.
+/// This cannot be used on its own.
 abstract class Motor extends Resource {
   static const Subtype subtype = Subtype(ResourceNamespaceRDK, ResourceTypeComponent, 'motor');
 
@@ -33,22 +37,40 @@ abstract class Motor extends Resource {
 
   Motor(this.name);
 
+  /// Sets the "percentage" of power the [Motor] should employ between -1 and 1.
+  /// When [powerPct] is negative, the rotation will be in the backward direction.
   Future<void> setPower(double powerPct, {Map<String, dynamic>? extra});
 
+  /// Spin the [Motor] the specified number of [revolutions] at specified [rpm].
+  /// When [rpm] or [revolutions] is a negative value, the rotation will be in the backward direction.
+  /// Note: if both [rpm] and [revolutions] are negative, the motor will spin in the forward direction.
   Future<void> goFor(double rpm, double revolutions, {Map<String, dynamic>? extra});
 
+  /// Spin the [Motor] to the specified position (provided in revolutions from home/zero),
+  /// at the specified speed, in revolutions per minute.
+  /// Regardless of the directionality of the [rpm] this function will move
+  /// the [Motor] towards the specified position.
   Future<void> goTo(double rpm, double positionRevolutions, {Map<String, dynamic>? extra});
 
+  /// Set the current position (modified by [offset]) to be the new zero (home) position.
   Future<void> resetZeroPosition(double offset, {Map<String, dynamic>? extra});
 
+  /// Report the position of the motor based on its encoder.
+  /// The value returned is the number of revolutions relative to its zero position.
+  /// This method will raise an exception if position reporting is not supported by the motor.
   Future<double> getPosition({Map<String, dynamic>? extra});
 
+  /// Report a dictionary mapping optional properties to
+  /// whether it is supported by this motor.
   Future<MotorProperties> getProperties({Map<String, dynamic>? extra});
 
+  /// Stop the motor immediately, without any gradual step down.
   Future<void> stop({Map<String, dynamic>? extra});
 
+  /// Returns whether or not the motor is currently running.
   Future<PowerState> isPowered({Map<String, dynamic>? extra});
 
+  /// Get if the [Motor] is currently moving.
   Future<bool> isMoving({Map<String, dynamic>? extra});
 
   static ResourceName getResourceName(String name) {
