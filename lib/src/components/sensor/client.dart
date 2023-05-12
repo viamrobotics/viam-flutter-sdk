bb6f36b0a7c5a85169aaea1eeebd2ddd5fc47b8e
Clint <clint.purser@viam.com>
2023-05-12 11:32:41 -0400

    Sensor & Movement Sensor rename (#18)
    * sensor & Movement Sensor rename
    
    renaming methods in sensor and movement sensor to match dart style guide. (Avoid starting method names with 'get')

--- lib/src/components/sensor/client.dart
+++ lib/src/components/sensor/client.dart
@@ -11,7 +11,7 @@ class SensorClient extends Sensor {
   SensorClient(super.name, this._channel) : _client = SensorServiceClient(_channel);
 
   @override
-  Future<Map<String, dynamic>> getReadings({Map<String, dynamic>? extra}) async {
+  Future<Map<String, dynamic>> readings({Map<String, dynamic>? extra}) async {
     final response = await _client.getReadings(GetReadingsRequest(name: name, extra: extra?.toStruct()));
     return response.readings.map((key, value) => MapEntry(key, value.toPrimitive()));
   }
