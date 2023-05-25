d9b1ec436d22b3476dbc145397c5ed04a1d45483
Clint Purser <clintpurser@gmail.com>
2023-05-25 11:20:13 -0400

    remove unnecessary type annotations

--- lib/src/components/movement_sensor/service.dart
+++ lib/src/components/movement_sensor/service.dart
@@ -21,64 +21,64 @@ class MovementSensorService extends MovementSensorServiceBase {
 
   @override
   Future<DoCommandResponse> doCommand(ServiceCall call, DoCommandRequest request) async {
-    final MovementSensor movementSensor = _fromManager(request.name);
+    final movementSensor = _fromManager(request.name);
     final result = await movementSensor.doCommand(request.command.toMap());
     return DoCommandResponse(result: result.toStruct());
   }
 
   @override
   Future<GetAccuracyResponse> getAccuracy(ServiceCall call, GetAccuracyRequest request) async {
-    final MovementSensor movementSensor = _fromManager(request.name);
+    final movementSensor = _fromManager(request.name);
     final accuracyMm = await movementSensor.accuracy(extra: request.extra.toMap());
     return GetAccuracyResponse(accuracyMm: accuracyMm);
   }
 
   @override
   Future<GetAngularVelocityResponse> getAngularVelocity(ServiceCall call, GetAngularVelocityRequest request) async {
-    final MovementSensor movementSensor = _fromManager(request.name);
-    final Vector3 angularVelocity = await movementSensor.angularVelocity(extra: request.extra.toMap());
+    final movementSensor = _fromManager(request.name);
+    final angularVelocity = await movementSensor.angularVelocity(extra: request.extra.toMap());
     return GetAngularVelocityResponse(angularVelocity: angularVelocity);
   }
 
   @override
   Future<GetCompassHeadingResponse> getCompassHeading(ServiceCall call, GetCompassHeadingRequest request) async {
-    final MovementSensor movementSensor = _fromManager(request.name);
-    final double value = await movementSensor.compassHeading(extra: request.extra.toMap());
+    final movementSensor = _fromManager(request.name);
+    final value = await movementSensor.compassHeading(extra: request.extra.toMap());
     return GetCompassHeadingResponse(value: value);
   }
 
   @override
   Future<GetLinearAccelerationResponse> getLinearAcceleration(ServiceCall call, GetLinearAccelerationRequest request) async {
-    final MovementSensor movementSensor = _fromManager(request.name);
-    final Vector3 linearAcceleration = await movementSensor.linearAcceleration(extra: request.extra.toMap());
+    final movementSensor = _fromManager(request.name);
+    final linearAcceleration = await movementSensor.linearAcceleration(extra: request.extra.toMap());
     return GetLinearAccelerationResponse(linearAcceleration: linearAcceleration);
   }
 
   @override
   Future<GetLinearVelocityResponse> getLinearVelocity(ServiceCall call, GetLinearVelocityRequest request) async {
-    final MovementSensor movementSensor = _fromManager(request.name);
-    final Vector3 linearVelocity = await movementSensor.linearVelocity(extra: request.extra.toMap());
+    final movementSensor = _fromManager(request.name);
+    final linearVelocity = await movementSensor.linearVelocity(extra: request.extra.toMap());
     return GetLinearVelocityResponse(linearVelocity: linearVelocity);
   }
 
   @override
   Future<GetOrientationResponse> getOrientation(ServiceCall call, GetOrientationRequest request) async {
-    final MovementSensor movementSensor = _fromManager(request.name);
-    final Orientation orientation = await movementSensor.orientation(extra: request.extra.toMap());
+    final movementSensor = _fromManager(request.name);
+    final orientation = await movementSensor.orientation(extra: request.extra.toMap());
     return GetOrientationResponse(orientation: orientation);
   }
 
   @override
   Future<GetPositionResponse> getPosition(ServiceCall call, GetPositionRequest request) async {
-    final MovementSensor movementSensor = _fromManager(request.name);
-    final Position position = await movementSensor.position(extra: request.extra.toMap());
+    final movementSensor = _fromManager(request.name);
+    final position = await movementSensor.position(extra: request.extra.toMap());
     return GetPositionResponse(coordinate: position.coordinates, altitudeMm: position.altitude);
   }
 
   @override
   Future<GetPropertiesResponse> getProperties(ServiceCall call, GetPropertiesRequest request) async {
-    final MovementSensor movementSensor = _fromManager(request.name);
-    final Properties properties = await movementSensor.properties(extra: request.extra.toMap());
+    final movementSensor = _fromManager(request.name);
+    final properties = await movementSensor.properties(extra: request.extra.toMap());
     return GetPropertiesResponse(
         linearVelocitySupported: properties.linearVelocitySupported,
         angularVelocitySupported: properties.angularVelocitySupported,
