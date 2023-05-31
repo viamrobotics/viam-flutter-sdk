af101dea4c6aca90916753a942452fd14a4b6d4f
Clint Purser <clintpurser@gmail.com>
2023-05-31 17:32:42 -0400

    add doCommand

--- lib/src/components/sensor/client.dart
+++ lib/src/components/sensor/client.dart
@@ -1,5 +1,6 @@
 import 'package:grpc/grpc_connection_interface.dart';
 
+import '../../gen/common/v1/common.pb.dart';
 import '../../gen/component/sensor/v1/sensor.pbgrpc.dart';
 import '../../utils.dart';
 import 'sensor.dart';
@@ -17,5 +18,11 @@ class SensorClient extends Sensor {
   Future<Map<String, dynamic>> readings({Map<String, dynamic>? extra}) async {
     final response = await _client.getReadings(GetReadingsRequest(name: name, extra: extra?.toStruct()));
     return response.readings.map((key, value) => MapEntry(key, value.toPrimitive()));
+  }
+
+  @override
+  Future<Map<String, dynamic>> doCommand(Map<String, dynamic> command) async {
+    final response = await _client.doCommand(DoCommandRequest(name: name, command: command.toStruct()));
+    return response.result.toMap();
   }
 }
