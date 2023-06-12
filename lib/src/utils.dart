f35d3c486142dad7085265c05389339d14d8fca5
Clint Purser <clintpurser@gmail.com>
2023-06-12 16:00:41 -0400

    throw error

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -17,20 +17,14 @@ extension NullableStringUtils on String? {
 
 extension ValueUtils on Value {
   dynamic toPrimitive() {
-    if (hasNumberValue()) {
-      return numberValue;
-    } else if (hasStringValue()) {
-      return stringValue;
-    } else if (hasBoolValue()) {
-      return boolValue;
-    } else if (hasStructValue()) {
-      return structValue.fields.map((key, value) => MapEntry(key, value.toPrimitive()));
-    } else if (hasListValue()) {
-      return listValue.values.map((e) => e.toPrimitive());
-    } else {
-      // default to null if no value found
-      return null;
-    }
+    if (hasBoolValue()) return boolValue;
+    if (hasListValue()) return listValue.values.map((e) => e.toPrimitive());
+    if (hasNullValue()) return nullValue;
+    if (hasNumberValue()) return numberValue;
+    if (hasStringValue()) return stringValue;
+    if (hasStructValue()) return structValue.fields.map((key, value) => MapEntry(key, value.toPrimitive()));
+
+    throw GrpcError.invalidArgument('value does not contain valid type');
   }
 }
 
@@ -43,26 +37,15 @@ extension StructUtils on Struct {
 extension ListValueUtils<T> on List<T> {
   Value toValue() {
     final values = map((e) {
-      try {
-        if (e is num) {
-          return Value(numberValue: e.toDouble());
-        } else if (e is String) {
-          return Value(stringValue: e);
-        } else if (e is bool) {
-          return Value(boolValue: e);
-        } else if (e is List<dynamic>) {
-          return e.toValue();
-        } else if (e is Map<String, dynamic>) {
-          return e.toValue();
-        } else if (e == null) {
-          return Value(nullValue: null);
-        } else {
-          throw GrpcError.invalidArgument('List contains unsupported type');
-        }
-      } catch (error, st) {
-        _logger.e('Error converting the List to a Value', error, st);
-        rethrow;
-      }
+      if (e is bool) return Value(boolValue: e);
+      if (e is List<dynamic>) return e.toValue();
+      if (e is Map<String, dynamic>) return e.toValue();
+      if (e is num) return Value(numberValue: e.toDouble());
+      if (e is String) return Value(stringValue: e);
+      if (e == null) return Value(nullValue: null);
+
+      _logger.e('Error converting the List to a Value');
+      throw GrpcError.invalidArgument('List contains unsupported type');
     });
 
     return Value(listValue: ListValue(values: values));
@@ -73,26 +56,22 @@ extension MapStructUtils on Map<String, dynamic> {
   Struct toStruct() {
     final Map<String, Value> result = {};
     for (final entry in entries) {
-      try {
-        final value = entry.value;
-        if (value is num) {
-          result[entry.key] = Value(numberValue: value.toDouble());
-        } else if (value is String) {
-          result[entry.key] = Value(stringValue: value);
-        } else if (value is bool) {
-          result[entry.key] = Value(boolValue: value);
-        } else if (value is List<dynamic>) {
-          result[entry.key] = value.toValue();
-        } else if (value is Map<String, dynamic>) {
-          result[entry.key] = value.toValue();
-        } else if (value == null) {
-          result[entry.key] = Value(nullValue: value);
-        } else {
-          throw GrpcError.invalidArgument('Unsupported type');
-        }
-      } catch (error, st) {
-        _logger.e('Error converting the Map to a Struct', error, st);
-        rethrow;
+      final value = entry.value;
+      if (value is num) {
+        result[entry.key] = Value(numberValue: value.toDouble());
+      } else if (value is String) {
+        result[entry.key] = Value(stringValue: value);
+      } else if (value is bool) {
+        result[entry.key] = Value(boolValue: value);
+      } else if (value is List<dynamic>) {
+        result[entry.key] = value.toValue();
+      } else if (value is Map<String, dynamic>) {
+        result[entry.key] = value.toValue();
+      } else if (value == null) {
+        result[entry.key] = Value(nullValue: value);
+      } else {
+        _logger.e('Error converting the Map to a Struct');
+        throw GrpcError.invalidArgument('Unsupported type');
       }
     }
     return Struct(fields: result);
