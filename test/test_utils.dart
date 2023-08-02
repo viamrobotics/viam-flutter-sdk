5d0865aa4354792a6e5424b5a1f7c68de64cf4e1
Naveed Jooma <naveed@viam.com>
2023-08-02 17:19:13 -0400

    Sensor widget tests

--- test/test_utils.dart
+++ test/test_utils.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+
 int generateTestingPortFromName(String name) => 50000 + (name.hashCode % 10000);
+
+class TestableWidget extends StatelessWidget {
+  const TestableWidget({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        body: child,
+      ),
+    );
+  }
+}
