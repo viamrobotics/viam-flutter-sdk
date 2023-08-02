2898b146a60e54b6e292c001ab052d96b1557699
Naveed Jooma <naveed@viam.com>
2023-08-02 17:23:28 -0400

    Additional test

--- test/widget_tests/sensor_widget_test.dart
+++ test/widget_tests/sensor_widget_test.dart
@@ -16,6 +16,32 @@ void main() {
       expect(dataTable, findsOneWidget);
     });
 
+    testWidgets('shows last refresh time', (tester) async {
+      final widget = TestableWidget(child: ViamSensorWidget(sensor: FakeSensor('sensor'), showLastRefreshed: true));
+      await tester.pumpWidget(widget);
+
+      final refreshButton = find.widgetWithIcon(ViamButton, Icons.refresh);
+      await tester.press(refreshButton);
+      await tester.pump();
+
+      final refreshTime = find.textContaining(RegExp(r'Updated at:.*'));
+
+      expect(refreshTime, findsOneWidget);
+    });
+
+    testWidgets('hides last refresh time', (tester) async {
+      final widget = TestableWidget(child: ViamSensorWidget(sensor: FakeSensor('sensor'), showLastRefreshed: false));
+      await tester.pumpWidget(widget);
+
+      final refreshButton = find.widgetWithIcon(ViamButton, Icons.refresh);
+      await tester.press(refreshButton);
+      await tester.pump();
+
+      final refreshTime = find.textContaining(RegExp(r'Updated at:.*'));
+
+      expect(refreshTime, findsNothing);
+    });
+
     testWidgets('shows refresh controls', (tester) async {
       final widget = TestableWidget(child: ViamSensorWidget(sensor: FakeSensor('sensor'), showRefreshControls: true));
       await tester.pumpWidget(widget);
@@ -48,30 +74,15 @@ void main() {
       expect(refreshButton, findsNothing);
     });
 
-    testWidgets('shows last refresh time', (tester) async {
-      final widget = TestableWidget(child: ViamSensorWidget(sensor: FakeSensor('sensor'), showLastRefreshed: true));
+    testWidgets('hides play/pause when refresh interval is null', (tester) async {
+      final widget = TestableWidget(child: ViamSensorWidget(sensor: FakeSensor('sensor'), refreshInterval: null));
       await tester.pumpWidget(widget);
 
-      final refreshButton = find.widgetWithIcon(ViamButton, Icons.refresh);
-      await tester.press(refreshButton);
-      await tester.pump();
+      final playButton = find.widgetWithIcon(ViamButton, Icons.play_arrow);
+      final pauseButton = find.widgetWithIcon(ViamButton, Icons.pause);
 
-      final refreshTime = find.textContaining(RegExp(r'Updated at:.*'));
-
-      expect(refreshTime, findsOneWidget);
-    });
-
-    testWidgets('hides last refresh time', (tester) async {
-      final widget = TestableWidget(child: ViamSensorWidget(sensor: FakeSensor('sensor'), showLastRefreshed: false));
-      await tester.pumpWidget(widget);
-
-      final refreshButton = find.widgetWithIcon(ViamButton, Icons.refresh);
-      await tester.press(refreshButton);
-      await tester.pump();
-
-      final refreshTime = find.textContaining(RegExp(r'Updated at:.*'));
-
-      expect(refreshTime, findsNothing);
+      expect(playButton, findsNothing);
+      expect(pauseButton, findsNothing);
     });
   });
 }
