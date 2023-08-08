467c330829f31f075b3966f654a159bb6e92bfc8
Naveed Jooma <naveed@viam.com>
2023-08-08 12:54:15 -0400

    Document viam

--- lib/src/viam_sdk.dart
+++ lib/src/viam_sdk.dart
@@ -4,13 +4,19 @@ import './gen/app/v1/app.pbgrpc.dart';
 import './robot/client.dart';
 import './viam_sdk_impl.dart';
 
+/// An object to interact with the Viam app
 abstract class Viam {
+  /// Create an authenticated Viam instance with the provided [accessToken]
   static Viam withAccessToken(String accessToken) {
     return ViamImpl.withAccessToken(accessToken);
   }
 
+  /// A client to communicate with Viam's app service
   AppClient get appClient;
+
+  /// A client to communicate with Viam's data and data sync services
   DataClient get dataClient;
 
+  /// Get a [RobotClient] connected to the provided [Robot]
   Future<RobotClient> getRobotClient(Robot robot);
 }
