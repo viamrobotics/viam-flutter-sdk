4c7b1dfecb7a83dc7296142e05cf998283be3f22
Naveed Jooma <naveed@viam.com>
2023-08-24 16:20:04 -0400

    Dialog error

--- lib/widgets/camera_stream.dart
+++ lib/widgets/camera_stream.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'package:logger/logger.dart';
 import 'package:viam_sdk/viam_sdk.dart';
@@ -22,6 +22,7 @@ class ViamCameraStreamView extends StatefulWidget {
 class _ViamCameraStreamViewState extends State<ViamCameraStreamView> {
   late RTCVideoRenderer _renderer;
   late StreamSubscription<MediaStream> _streamSub;
+  bool _displayingError = false;
 
   @override
   void initState() {
@@ -42,14 +43,43 @@ class _ViamCameraStreamViewState extends State<ViamCameraStreamView> {
     await _renderer.initialize();
     final stream = widget.streamClient.getStream();
     _streamSub = stream.listen((event) {
+      if (_displayingError) {
+        _displayingError = false;
+        Navigator.pop(context);
+      }
       _renderer.srcObject = event;
       setState(() {});
     });
-    _streamSub.onError((error, trace) => Logger().e(
-          'Encountered a stream error for camera ${widget.camera.name}',
-          error: error,
-          stackTrace: trace,
-        ));
+    _streamSub.onError((error, trace) {
+      Logger().e(
+        'Encountered a stream error for camera ${widget.camera.name}',
+        error: error,
+        stackTrace: trace,
+      );
+      if (_displayingError) {
+        return null;
+      }
+      _displayingError = true;
+      return showDialog<void>(
+          barrierDismissible: false,
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+                title: Text('Error connecting to camera: ${widget.camera.name}'),
+                content: Text(
+                  'Error: $error',
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text('Go Back'),
+                    onPressed: () {
+                      int count = 0;
+                      Navigator.popUntil(context, (route) => count++ >= 2);
+                    },
+                  ),
+                ]);
+          });
+    });
   }
 
   @override
