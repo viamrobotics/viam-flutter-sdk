679feb6e7189e0fc2519bd3ccd0e6856083ae1ac
Clint Purser <clintpurser@gmail.com>
2024-01-03 17:08:21 -0500

    update example to use new imageData api

--- example/viam_robot_example_app/lib/screens/stream.dart
+++ example/viam_robot_example_app/lib/screens/stream.dart
@@ -1,8 +1,6 @@
 import 'dart:typed_data';
-import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
-import 'package:image/image.dart' as img;
 import 'package:viam_sdk/viam_sdk.dart';
 import 'package:viam_sdk/widgets.dart';
 
@@ -21,48 +19,18 @@ class StreamScreen extends StatefulWidget {
 
 class _StreamScreenState extends State<StreamScreen> {
   // Single frame
-  ByteData? imageBytes;
+  Uint8List? _imageData;
   bool _imgLoaded = false;
 
-  void _getImage() {
+  Future<void> _getImage() async {
     setState(() {
       _imgLoaded = false;
     });
-    final imageFut = widget.camera.image();
-    imageFut.then((value) {
-      final convertFut = convertImageToFlutterUi(value.image ?? img.Image.empty());
-      convertFut.then((value) {
-        final pngFut = value.toByteData(format: ui.ImageByteFormat.png);
-        pngFut.then((value) => setState(() {
-              imageBytes = value;
-              _imgLoaded = true;
-            }));
-      });
+    final imageDataResponse = await widget.camera.imageData();
+    setState(() {
+      _imageData = imageDataResponse;
+      _imgLoaded = true;
     });
-  }
-
-  Future<ui.Image> convertImageToFlutterUi(img.Image image) async {
-    if (image.format != img.Format.uint8 || image.numChannels != 4) {
-      final cmd = img.Command()
-        ..image(image)
-        ..convert(format: img.Format.uint8, numChannels: 4);
-      final rgba8 = await cmd.getImageThread();
-      if (rgba8 != null) {
-        image = rgba8;
-      }
-    }
-
-    final ui.ImmutableBuffer buffer = await ui.ImmutableBuffer.fromUint8List(image.toUint8List());
-
-    final ui.ImageDescriptor id =
-        ui.ImageDescriptor.raw(buffer, height: image.height, width: image.width, pixelFormat: ui.PixelFormat.rgba8888);
-
-    final ui.Codec codec = await id.instantiateCodec(targetHeight: image.height, targetWidth: image.width);
-
-    final ui.FrameInfo fi = await codec.getNextFrame();
-    final ui.Image uiImage = fi.image;
-
-    return uiImage;
   }
 
   @override
@@ -82,7 +50,7 @@ class _StreamScreenState extends State<StreamScreen> {
             const SizedBox(height: 16),
             ViamCameraStreamView(camera: widget.camera, streamClient: widget.client),
             const SizedBox(height: 16),
-            if (_imgLoaded) Image.memory(Uint8List.view(imageBytes!.buffer), scale: 3),
+            if (_imgLoaded) Image.memory(_imageData!, scale: 3),
             const SizedBox(height: 16),
             ElevatedButton(
               child: const Text('Get image'),
