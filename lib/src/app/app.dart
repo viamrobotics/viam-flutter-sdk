381edd55142e22cc83e4e0ed80d9630d2c2b48f4
purplenicole730 <jihyun.nicole.jung@gmail.com>
2024-05-07 12:37:05 -0400

    add robot wrappers

--- lib/src/app/app.dart
+++ lib/src/app/app.dart
@@ -16,25 +16,6 @@ class AppClient {
 
   AppClient(this._client);
 
-  // getRobot - DONE
-  // getRoverRentalRobots
-  // getRobotParts - DONE
-  // getRobotPart - DONE
-  // getRobotPartLogs - DONE
-  // tailRobotPartLogs - DONE
-  // getRobotPartHistory
-  // updateRobotPart - DONE
-  // newRobotPart
-  // deleteRobotPart
-  // getRobotAPIKeys
-  // markPartAsMain
-  // markPartForRestart
-  // createRobotPartSecret
-  // deleteRobotPartSecret
-  // listRobots - DONE
-  // newRobot - DONE
-  // updateRobot
-  // deleteRobot
   // listFragments
   // getFragment - DONE
   // createFragment
@@ -274,18 +255,18 @@ class AppClient {
     await _client.deleteLocationSecret(request);
   }
 
-  /// List the [Robot] of a specific [Location] that the currently authenticated user has access to
-  Future<List<Robot>> listRobots(Location location) async {
-    final listRobotsRequest = ListRobotsRequest()..locationId = location.id;
-    final ListRobotsResponse response = await _client.listRobots(listRobotsRequest);
-    return response.robots;
-  }
-
   /// Get a specific [Robot] by ID
   Future<Robot> getRobot(String robotId) async {
     final getRobotRequest = GetRobotRequest()..id = robotId;
     final GetRobotResponse response = await _client.getRobot(getRobotRequest);
     return response.robot;
+  }
+
+  /// Get [RoverRentalRobot]s in an [Organization]
+  Future<RoverRentalRobot> getRoverRentalRobots(String orgId) async {
+    final GetRoverRentalRobotsRequest()..orgId = orgId;
+    final GetRoverRentalRobotsResponse response = await _client.getRoverRentalRobots(request);
+    return response.robots;
   }
 
   /// List the [RobotPart] of a specific [Robot] that the currently authenticated user has access to
@@ -299,16 +280,6 @@ class AppClient {
   Future<RobotPart> getRobotPart(String partId) async {
     final getRobotPartRequest = GetRobotPartRequest()..id = partId;
     final response = await _client.getRobotPart(getRobotPartRequest);
-    return response.part;
-  }
-
-  /// Update a specific [RobotPart] by ID
-  Future<RobotPart> updateRobotPart(String partId, String name, Struct robotConfig) async {
-    final updateRobotPartRequest = UpdateRobotPartRequest()
-      ..id = partId
-      ..name = name
-      ..robotConfig = robotConfig;
-    final response = await _client.updateRobotPart(updateRobotPartRequest);
     return response.part;
   }
 
@@ -331,6 +302,105 @@ class AppClient {
     return stream.asBroadcastStream(onCancel: (_) => response.cancel());
   }
 
+  /// Get a specific [RobotPart] history by ID
+  Future<List<HistoryEntry>> getRobotPartHistory(String id) async {
+    final request = GetRobotPartHistoryRequest()..id = id;
+    final GetRobotPartHistoryResponse response = await _client.getRobotPartHistory(request);
+    return response.history;
+  }
+
+  /// Update a specific [RobotPart] by ID
+  Future<RobotPart> updateRobotPart(String partId, String name, Struct robotConfig) async {
+    final updateRobotPartRequest = UpdateRobotPartRequest()
+      ..id = partId
+      ..name = name
+      ..robotConfig = robotConfig;
+    final response = await _client.updateRobotPart(updateRobotPartRequest);
+    return response.part;
+  }
+
+  /// Create a new [RobotPart]
+  Future<String> newRobotPart(String robotId, String partName) async {
+    final request = NewRobotPartRequest()
+      ..robotId = robotId
+      ..partName = partName;
+    final NewRobotPartResponse response = await _client.newRobotPart(request);
+    return response.partId;
+  }
+
+  /// Delete a [RobotPart]
+  Future<void> deleteRobotPart(String partId) async {
+    final request = DeleteRobotPartRequest()..partId = partId;
+    await _client.deleteRobotPart(request);
+  }
+
+  /// Gets the [APIKey]'s for a [Robot]
+  Future<List<APIKeyWithAuthorizations>> getRobotAPIKeys(String robotId) async {
+    final request = GetRobotAPIKeysRequest()..robotId = robotId;
+    final GetRobotAPIKeysResponse response = await _client.getRobotAPIKeys(request);
+    return response.apiKeys;
+  }
+  
+  /// Marks the given [RobotPart] as the main part, and all the others as not
+  Future<void> markPartAsMain(String partId) async {
+    final request = MarkPartAsMainRequest()..partId = partId;
+    await _client.markPartAsMain(request);
+  }
+
+  /// Marks [RobotPart] for restart. Once the [RobotPart] checks-in with the app the flag is reset on the [RobotPart]. Calling this multiple times before a [RobotPart] checks-in has no affect.
+  /// Note: This API may be removed in the near future.
+  Future<void> markPartForRestart(String partId) async {
+    final request = markPartForRestart()..partId = partId;
+    await _client.markPartForRestart(request);
+  }
+
+  /// Create a new generated Secret in the [RobotPart]. Succeeds if there are no more than 2 active secrets after creation.
+  Future<RobotPart> createRobotPartSecret(String partId) aync {
+    final request = createRobotPartSecret()..partId = partId;
+    final CreateRobotPartSecretResponse response = await _client.createRobotPartSecret(request);
+    return response.part;
+  }
+
+  /// Delete a Secret from the [RobotPart].
+  Future<void> deleteRobotPartSecret(String partId, String secretId) async {
+    final request = DeleteRobotPartSecretRequest()
+      ..partId = partId
+      ..secretId = secretId;
+    await _client.deleteRobotPartSecret(request);
+  }
+
+  /// List the [Robot] of a specific [Location] that the currently authenticated user has access to
+  Future<List<Robot>> listRobots(Location location) async {
+    final listRobotsRequest = ListRobotsRequest()..locationId = location.id;
+    final ListRobotsResponse response = await _client.listRobots(listRobotsRequest);
+    return response.robots;
+  }
+
+  /// Create a new smart machine with the included [name] in the passed in [locationId]
+  Future<String> newMachine(String name, String locationId) async {
+    final request = NewRobotRequest()
+      ..name = name
+      ..location = locationId;
+    final response = await _client.newRobot(request);
+    return response.id;
+  }
+
+  /// Update a [Robot]
+  Future<Robot> updateRobot(String id, String name, String location) async {
+    final request = UpdateRobotRequest()
+      ..id = id
+      ..name = name
+      ..location = location;
+    final UpdateRobotResponse response = await _client.updateRobot(request);
+    return response.robot;
+  }
+
+  /// Delete a [Robot]
+  Future<void> deleteRobot(String id) async {
+    final request = DeleteRobotRequest()..id = id;
+    final DeleteRobotResponse response = await _client.deleteRobot(request);
+  }
+
   /// List the [Authorization]s available for the currently authenticated user
   Future<List<Authorization>> listAuthorizations(String organizationId, {List<String> resourceIds = const []}) async {
     final request = ListAuthorizationsRequest()
@@ -351,15 +421,6 @@ class AppClient {
     return response.authorizedPermissions.first.permissions
         .map((e) => Permission.values.firstWhere((element) => element.value == e))
         .toList();
-  }
-
-  /// Create a new smart machine with the included [name] in the passed in [locationId]
-  Future<String> newMachine(String name, String locationId) async {
-    final request = NewRobotRequest()
-      ..name = name
-      ..location = locationId;
-    final response = await _client.newRobot(request);
-    return response.id;
   }
 
   /// Get a specific [Fragment] by id.
