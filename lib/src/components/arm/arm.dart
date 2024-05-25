0e128386e8630d06f7fd17ac33477816f1a1c80f
JessamyT <jessamy.taylor@gmail.com>
2024-05-24 18:17:50 -0700

    DOCS-2031: Add arm, base, board example snippets

--- lib/src/components/arm/arm.dart
+++ lib/src/components/arm/arm.dart
@@ -7,24 +7,60 @@ abstract class Arm extends Resource {
   static const Subtype subtype = Subtype(resourceNamespaceRDK, resourceTypeComponent, 'arm');
 
   /// Get the current [Pose] of the end of the arm.
+  ///
+  /// ```
+  /// // Get the pose of an arm named "myArm"
+  /// const currentPose = await myArm.endPosition();
+  /// ```
   Future<Pose> endPosition({Map<String, dynamic>? extra});
 
   /// Move the end of the arm to the [Pose] specified.
+  ///
+  /// ```
+  /// // Create a pose for the arm to move to
+  /// final Pose targetPose = Pose(12, 0, 400, 0, 0, 1);
+  ///
+  /// // Move the arm to the pose
+  /// await myArm.moveToPosition(targetPose);
+  /// ```
   Future<void> moveToPosition(Pose pose, {Map<String, dynamic>? extra});
 
-  /// Move each joint on the arm to the corresponding position specified in [positions]
+  /// Move each joint on the arm to the corresponding position specified in [positions].
+  ///
+  /// ```
+  /// // Create a list of joint angles for each arm joint
+  /// List<double> targetPositions = [180, 90, 15.75, 30, 90, 0];
+  ///
+  /// // Move the arm joints to those angles
+  /// await myArm.moveToJointPosition(targetPositions);
+  /// ```
   Future<void> moveToJointPositions(List<double> positions, {Map<String, dynamic>? extra});
 
   /// Get the [List] representing the current position of the arm
+  ///
+  /// ```
+  /// // Get the current angles of all joints of the arm
+  /// List<double> currentJointPositions = await myArm.moveToJointPosition();
+  /// ```
   Future<List<double>> jointPositions({Map<String, dynamic>? extra});
 
   /// Stops all motion of the arm. It is assumed that the arm stops immediately.
+  ///
+  /// ```
+  /// // Stop the arm's motion
+  /// await myArm.stop();
+  /// ```
   Future<void> stop({Map<String, dynamic>? extra});
 
   /// Get if the arm is currently moving
+  ///
+  /// ```
+  /// // Check whether the arm is currently moving
+  /// bool isArmMoving = await myArm.isMoving();
+  /// ```
   Future<bool> isMoving();
 
-  /// Get the [ResourceName] for this [Arm] with the given [name]
+  /// Get the [ResourceName] for this [Arm] with the given [name].
   static ResourceName getResourceName(String name) {
     return Arm.subtype.getResourceName(name);
   }
--- lib/src/components/board/board.dart
+++ lib/src/components/board/board.dart
@@ -15,39 +15,101 @@ abstract class Board extends Resource {
   static const Subtype subtype = Subtype(resourceNamespaceRDK, resourceTypeComponent, 'board');
 
   /// Set the high/low state of the given pin of a board.
+  ///
+  /// ```
+  /// // Set pin 15 to high
+  /// await myBoard.setGpioState("15", true);
+  /// ```
   Future<void> setGpioState(String pin, bool high, {Map<String, dynamic>? extra});
 
   /// Get the high/low state of the given pin of a board.
+  ///
+  /// ```
+  /// // Get the state of pin 15
+  /// bool pinStateIsHigh = await myBoard.gpio("15");
+  /// ```
   Future<bool> gpio(String pin, {Map<String, dynamic>? extra});
 
   /// Get the duty cycle of the given pin of a board.
+  ///
+  /// ```
+  /// // Get the PWM duty cycle of pin 15
+  /// var dutyCycle = await myBoard.pwm("15");
+  /// ```
   Future<double> pwm(String pin, {Map<String, dynamic>? extra});
 
   /// Set the duty cycle of the given pin of a board.
+  ///
+  /// ```
+  /// // Set the PWM duty cycle of pin 13
+  /// await myBoard.setPwm("13");
+  /// ```
   Future<void> setPwm(String pin, double dutyCyclePct, {Map<String, dynamic>? extra});
 
   /// Get the PWM frequency of the given pin of a board.
+  ///
+  /// ```
+  /// // Get the PWM frequency of pin 11
+  /// var frequency = await myBoard.get("11");
+  /// ```
   Future<int> pwmFrequency(String pin, {Map<String, dynamic>? extra});
 
-  /// Set the PWM frequency of the given pin of a board.
+  /// Set the PWM frequency in Hertz of the given pin of a board.
+  ///
+  /// ```
+  /// // Set the PWM frequency of pin 15 to 1600 Hz
+  /// await myBoard.setPwmFrequency("15", 1600);
+  /// ```
   Future<void> setPwmFrequency(String pin, int frequencyHz, {Map<String, dynamic>? extra});
 
   /// Read the current value of an analog reader of a board.
+  ///
+  /// ```
+  /// // Get the current value of an analog reader named "my_example_analog"
+  /// var analogVal = await myBoard.analogReaderValue("my_example_analog");
+  /// ```
   Future<AnalogValue> analogReaderValue(String analogReaderName, {Map<String, dynamic>? extra});
 
   /// Return the current value of the interrupt which is based on the type of Interrupt.
+  ///
+  /// ```
+  /// // Get the current value of a digital interrupt named "my_example_digital_interrupt"
+  /// var interruptVal = await myBoard.digitalInterruptValue("my_example_digital_interrupt");
+  /// ```
   Future<int> digitalInterruptValue(String digitalInterruptName, {Map<String, dynamic>? extra});
 
-  // Stream digital interrupts ticks.
+  /// Stream digital interrupts ticks.
+  ///
+  /// ```
+  /// // Stream ticks from digital interrupts on pins 8 and 11
+  /// List<string> interrupts = ["8", "11"];
+  /// Stream<Tick> tickStream = await myBoard.streamTicks(interrupts);
+  /// ```
   Stream<Tick> streamTicks(List<String> interrupts, {Map<String, dynamic>? extra});
 
-  // addCallbacks adds a listener for the digital interrupts.
+  /// addCallbacks adds a listener for the digital interrupts.
+  ///
+  /// ```
+  /// List<string> interrupts = ["8", "11"];
+  /// Queue<Tick> tickQueue;
+  /// await myBoard.addCallbacks(interrupts, tickQueue);
+  /// ```
   Future<void> addCallbacks(List<String> interrupts, Queue<Tick> tickQueue, {Map<String, dynamic>? extra});
 
   /// Set the board to the indicated power mode.
+  ///
+  /// ```
+  /// // Set the power mode of the board to offline deep for 60 seconds
+  /// await myBoard.setPowerMode(POWER_MODE_OFFLINE_DEEP, 60, 0);
+  /// ```
   Future<void> setPowerMode(PowerMode powerMode, int seconds, int nanos, {Map<String, dynamic>? extra});
 
   /// Write analog value to pin.
+  ///
+  /// ```
+  /// // Set pin 11 to value 48
+  /// await myBoard.writeAnalog("11", 48);
+  /// ```
   Future<void> writeAnalog(String pin, int value, {Map<String, dynamic>? extra});
 
   /// Get the [ResourceName] for this [Board] with the given [name]
