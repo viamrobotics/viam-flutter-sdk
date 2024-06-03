fa9f2064160ac0c0e8d1b4f99c5d551696450c8f
JessamyT <75634662+JessamyT@users.noreply.github.com>
2024-06-03 16:43:38 -0700

    DOCS-2031: Add arm, base, board example snippets (#218)

--- lib/src/components/arm/arm.dart
+++ lib/src/components/arm/arm.dart
@@ -7,24 +7,57 @@ abstract class Arm extends Resource {
   static const Subtype subtype = Subtype(resourceNamespaceRDK, resourceTypeComponent, 'arm');
 
   /// Get the current [Pose] of the end of the arm.
+  ///
+  /// ```
+  /// // Get the pose of an arm named "myArm"
+  /// final currentPose = await myArm.endPosition();
+  /// ```
   Future<Pose> endPosition({Map<String, dynamic>? extra});
 
   /// Move the end of the arm to the [Pose] specified.
+  ///
+  /// ```
+  /// // Create a pose for the arm to move to
+  /// final targetPose = Pose.fromBuffer([12, 0, 400, 0, 0, 1, 90]);
+  ///
+  /// // Move the arm to the pose
+  /// await myArm.moveToPosition(targetPose);
+  /// ```
   Future<void> moveToPosition(Pose pose, {Map<String, dynamic>? extra});
 
-  /// Move each joint on the arm to the corresponding position specified in [positions]
+  /// Move each joint on the arm to the corresponding position specified in [positions].
+  ///
+  /// ```
+  /// // Create a list of joint angles for each arm joint
+  /// List<double> targetPositions = [180, 90, 15.75, 30, 90, 0];
+  ///
+  /// // Move the arm joints to those angles
+  /// await myArm.moveToJointPositions(targetPositions);
+  /// ```
   Future<void> moveToJointPositions(List<double> positions, {Map<String, dynamic>? extra});
 
-  /// Get the [List] representing the current position of the arm
+  /// Get the [List] of current joint angles of each arm joint
+  ///
+  /// ```
+  /// List<double> currentJointPositions = await myArm.moveToJointPosition();
+  /// ```
   Future<List<double>> jointPositions({Map<String, dynamic>? extra});
 
-  /// Stops all motion of the arm. It is assumed that the arm stops immediately.
+  /// Stop all motion of the arm. It is assumed that the arm stops immediately.
+  ///
+  /// ```
+  /// await myArm.stop();
+  /// ```
   Future<void> stop({Map<String, dynamic>? extra});
 
-  /// Get if the arm is currently moving
+  /// Whether the arm is currently moving
+  ///
+  /// ```
+  /// bool isArmMoving = await myArm.isMoving();
+  /// ```
   Future<bool> isMoving();
 
-  /// Get the [ResourceName] for this [Arm] with the given [name]
+  /// Get the [ResourceName] for this [Arm] with the given [name].
   static ResourceName getResourceName(String name) {
     return Arm.subtype.getResourceName(name);
   }
--- lib/src/components/base/base.dart
+++ lib/src/components/base/base.dart
@@ -14,12 +14,22 @@ abstract class Base extends Resource {
   ///
   /// When [distance] or [velocity] is 0, the [Base] will stop.
   /// This method blocks until completed or canceled.
+  ///
+  /// ```
+  /// // Move the base 40mm forward at 90 mm/s
+  /// await myBase.moveStraight(40, 90);
+  /// ```
   Future<void> moveStraight(int distance, double velocity, {Map<String, dynamic>? extra});
 
   /// Spin the [Base] in place [angle] degrees, at the given angular [velocity], expressed in degrees per second.
   ///
   /// When [velocity] is 0, the [Base] will stop.
   /// This method blocks until completed or canceled.
+  ///
+  /// ```
+  /// // Spin the base 10 degrees at 15 deg/s
+  /// await myBase.spin(10, 15);
+  /// ```
   Future<void> spin(double angle, double velocity, {Map<String, dynamic>? extra});
 
   /// Set the linear and angular velocity of the [Base].
@@ -29,15 +39,46 @@ abstract class Base extends Resource {
   /// When both [linear] and [angular] are zero-vectors, the base will stop.
   /// When [linear] and [angular] are both non-zero-vectors, the base will move in an arc,
   /// with a tighter radius if [angular] power is greater than [linear] power.
+  /// The positive y axis points forwards and the positive z axis points up.
+  ///
+  /// ```
+  /// // Move the base straight forward at 75% power:
+  /// await myBase.setPower(Vector3(0, 0.75, 0), Vector3());
+  ///
+  /// // Move the base straight backward at 100% power:
+  /// await myBase.setPower(Vector3(0, -1, 0), Vector3());
+  ///
+  /// // Turn the base to the left at 50% power:
+  /// await myBase.setPower(Vector3(), Vector3(0, 0, 0.5));
+  ///
+  /// // Turn the base to the right at 60% power:
+  /// await myBase.setPower(Vector3(), Vector3(0, 0, -0.6));
+  /// ```
   Future<void> setPower(Vector3 linear, Vector3 angular, {Map<String, dynamic>? extra});
 
-  /// Set the linear and angular velocities of the base, expressed as mm/sec vectors.
+  /// Set the linear and angular velocities of the base, expressed as
+  /// mm/sec and degrees/sec vectors.
+  ///
+  /// ```
+  /// // Set the linear velocity to 50mm/s forward, and the angular velocity
+  /// to 15 deg/s counterclockwise
+  /// //
+  /// await myBase.setVelocity(Vector3(0, 50, 0), Vector3(0, 0, 15));
+  /// ```
   Future<void> setVelocity(Vector3 linear, Vector3 angular, {Map<String, dynamic>? extra});
 
   /// Stop the base.
+  ///
+  /// ```
+  /// await myBase.stop();
+  /// ```
   Future<void> stop({Map<String, dynamic>? extra});
 
-  /// Get if the base is currently moving
+  /// Whether the base is currently moving
+  ///
+  /// ```
+  /// bool baseIsMoving = await myBase.isMoving();
+  /// ```
   Future<bool> isMoving();
 
   /// Get the [ResourceName] for this [Base] with the given [name]
