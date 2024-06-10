a5d65b3b407a2ac6cc0f270c5a2123d08b6eb534
JessamyT <75634662+JessamyT@users.noreply.github.com>
2024-06-10 10:35:20 -0700

    DOCS-2033: Add gripper example snippets (#227)

--- lib/src/components/gripper/gripper.dart
+++ lib/src/components/gripper/gripper.dart
@@ -7,15 +7,31 @@ abstract class Gripper extends Resource {
   static const Subtype subtype = Subtype(resourceNamespaceRDK, resourceTypeComponent, 'gripper');
 
   /// Open the [Gripper]
+  ///
+  /// ```
+  /// await myGripper.open();
+  /// ```
   Future<void> open({Map<String, dynamic>? extra});
 
   /// Close the [Gripper]
+  ///
+  /// ```
+  /// await myGripper.grab();
+  /// ```
   Future<void> grab({Map<String, dynamic>? extra});
 
   /// Stop all motion of the [Gripper]. It is assumed the [Gripper] stops immediately
+  ///
+  /// ```
+  /// await myGripper.stop();
+  /// ```
   Future<void> stop({Map<String, dynamic>? extra});
 
   /// If the [Gripper] is currently moving
+  ///
+  /// ```
+  /// var isItMoving = await myGripper.isMoving();
+  /// ```
   Future<bool> isMoving();
 
   /// Get the [ResourceName] for the [Gripper] with the given [name]
