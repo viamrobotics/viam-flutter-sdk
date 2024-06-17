542e18ba3c09fbf8bac04ad20c7c69f9b5114dd9
JessamyT <75634662+JessamyT@users.noreply.github.com>
2024-06-17 11:35:08 -0700

    DOCS-2035: Add example snippets to movement sensor, power sensor, sensor (#230)

--- lib/src/components/power_sensor/power_sensor.dart
+++ lib/src/components/power_sensor/power_sensor.dart
@@ -1,4 +1,3 @@
-import '../../components/sensor/sensor.dart';
 import '../../gen/common/v1/common.pb.dart';
 import '../../gen/component/powersensor/v1/powersensor.pb.dart';
 import '../../resource/base.dart';
@@ -11,25 +10,48 @@ typedef Current = GetCurrentResponse;
 abstract class PowerSensor extends Resource {
   static const Subtype subtype = Subtype(resourceNamespaceRDK, resourceTypeComponent, 'power_sensor');
 
-  /// Obtain the measurements/data specific to this [Sensor]
-  /// If a sensor is not configured to have a measurement or fails to read a piece of data, it will not appear in the readings dictionary.
+  /// Get the measurements or readings from this [PowerSensor].
+  /// If a sensor is not configured correctly or fails to read a
+  /// piece of data, it will not appear in the readings dictionary.
+  ///
+  /// ```
+  /// var readings = await myPowerSensor.readings();
+  /// ```
   Future<Map<String, dynamic>> readings({Map<String, dynamic>? extra});
 
-  /// Get the voltage (volts) and boolean isAC
+  /// Get the voltage in volts, and whether the power is
+  /// AC (true) or DC (false).
+  ///
+  /// ```
+  /// var voltageObject = await myPowerSensor.readings();
+  /// double voltageInVolts = voltageObject['volts'];
+  /// bool isItAC = voltageObject['isAc'];
+  /// ```
   Future<Voltage> voltage({Map<String, dynamic>? extra});
 
-  /// Get the current (amperes) and boolean isAC
+  /// Get the current in amperes, and whether the current
+  /// is AC (true) or DC (false).
+  ///
+  /// ```
+  /// var currentObject = await myPowerSensor.readings();
+  /// double amps = voltageObject['amperes'];
+  /// bool isItAC = voltageObject['isAc'];
+  /// ```
   Future<Current> current({Map<String, dynamic>? extra});
 
   /// Get the power (watts)
+  ///
+  /// ```
+  /// var power = await myPowerSensor.power();
+  /// ```
   Future<double> power({Map<String, dynamic>? extra});
 
-  /// Get the [ResourceName] for this [PowerSensor] with the given [name]
+  /// Get the [ResourceName] for this [PowerSensor] with the given [name].
   static ResourceName getResourceName(String name) {
     return PowerSensor.subtype.getResourceName(name);
   }
 
-  /// Get the [MovementSensor] named [name] from the provided robot.
+  /// Get the [PowerSensor] named [name] from the provided robot.
   static PowerSensor fromRobot(RobotClient robot, String name) {
     return robot.getResource(PowerSensor.getResourceName(name));
   }
--- lib/src/components/sensor/sensor.dart
+++ lib/src/components/sensor/sensor.dart
@@ -6,10 +6,14 @@ import '../../robot/client.dart';
 abstract class Sensor extends Resource {
   static const Subtype subtype = Subtype(resourceNamespaceRDK, resourceTypeComponent, 'sensor');
 
-  /// Obtain the measurements/data specific to this [Sensor]
+  /// Get the measurements or readings from this [Sensor].
+  ///
+  /// ```
+  /// var readings = await mySensor.readings();
+  /// ```
   Future<Map<String, dynamic>> readings({Map<String, dynamic>? extra});
 
-  /// Get the [ResourceName] for this [Sensor] with the given [name]
+  /// Get the [ResourceName] for this [Sensor] with the given [name].
   static ResourceName getResourceName(String name) {
     return Sensor.subtype.getResourceName(name);
   }
