c832369233d7148708753e3c2cba9e1f5dab4fb9
Benjamin Rewis <32186188+benjirewis@users.noreply.github.com>
2024-06-25 14:35:45 -0400

    RSDK-7769, RSDK-7770 Add AppRobotClient and LogOutput dial option (#233)

---
+++ lib/src/app/robot.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:logger/logger.dart';
+import 'package:viam_sdk/src/gen/google/protobuf/timestamp.pb.dart';
+
+import '../gen/app/v1/robot.pbgrpc.dart';
+import '../gen/common/v1/common.pb.dart';
+
+/// gRPC client for connecting to app's RobotService.
+///
+/// All calls must be authenticated.
+class AppRobotClient {
+  final RobotServiceClient _client;
+
+  AppRobotClient(this._client);
+
+  /// Log the OutputEvent to app with the given partId.
+  Future<void> log(String partId, host, loggerName, OutputEvent event) async {
+    late String level;
+    switch (event.level) {
+      case Level.debug:
+        level = 'debug';
+      case Level.warning:
+        level = 'warning';
+      case Level.error:
+        level = 'error';
+      default:
+        // Assume info level if none of the above.
+        level = 'info';
+    }
+
+    // Assume log was just output (OutputEvent has no timestamp field).
+    final Timestamp protoTs = Timestamp.fromDateTime(DateTime.now());
+
+    // Join lines with '\n' and suffix with '\n'.
+    final String message = '${event.lines.join('\n')}\n';
+
+    final LogEntry entry = LogEntry(host: host, level: level, time: protoTs, loggerName: loggerName, message: message);
+    final request = LogRequest(id: partId, logs: [entry]);
+    await _client.log(request);
+  }
+}
