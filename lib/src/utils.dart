86a98f0cdeefb35f545a4d06dec8457558b660d0
njooma <njooma@users.noreply.github.com>
2024-11-21 20:26:02 +0000

    Bump version to 0.0.28

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -116,7 +116,7 @@ extension GetReadingsResponseUtils on GetReadingsResponse {
 }
 
 String getVersionMetadata() {
-  const String sdkVersion = 'v0.0.27';
+  const String sdkVersion = 'v0.0.28';
   const String apiTag = 'v0.1.364';
 
   return 'flutter;$sdkVersion;$apiTag';
