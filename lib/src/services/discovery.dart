51762f2bf194d2342d31b1a068249d1f473c366e
martha-johnston <martha.johnston@viam.com>
2025-01-02 16:58:48 +0100

    add discovery service

---
+++ lib/src/services/discovery.dart
@@ -0,0 +1,56 @@
+import 'package:fixnum/fixnum.dart';
+import 'package:grpc/grpc_connection_interface.dart';
+
+import '../../protos/common/common.dart';
+import '../../protos/service/discovery.dart';
+import '../media/image.dart';
+import '../resource/base.dart';
+import '../robot/client.dart';
+import '../utils.dart';
+
+/// {@category Services}
+class DiscoveryClient extends Resource implements ResourceRPCClient {
+  static const Subtype subtype = Subtype(resourceNamespaceRDK, resourceTypeService, 'discovery');
+
+  @override
+  final String name;
+
+  @override
+  ClientChannelBase channel;
+
+  @override
+  DiscoveryServiceClient get client => DiscoveryServiceClient(channel);
+
+  DiscoveryClient(this.name, this.channel);
+
+  /// Returns a list of [ComponentConfig]s for all discovered viam resources connected to the viam-server machine.
+  ///
+  /// ```
+  /// // Example:
+  /// var resources = await myDiscoveryService.discoverResources('myWebcam');
+  /// ```
+  Future<List<ComponentConfig>> discoverResources(String discoveryName, {Map<String, dynamic>? extra}) async {
+    final request = DiscoverResourcesRequest(name: name, extra: extra?.toStruct());
+    final response = await client.discoverResources(request);
+    return response.discovery;
+  }
+
+  @override
+  Future<Map<String, dynamic>> doCommand(Map<String, dynamic> command) async {
+    final request = DoCommandRequest()
+      ..name = name
+      ..command = command.toStruct();
+    final response = await client.doCommand(request);
+    return response.result.toMap();
+  }
+
+  /// Get the [ResourceName] for this [DiscoveryClient] with the given [name]
+  static ResourceName getResourceName(String name) {
+    return DiscoveryClient.subtype.getResourceName(name);
+  }
+
+  /// Get the [DiscoveryClient] named [name] from the provided robot.
+  static DiscoveryClient fromRobot(RobotClient robot, String name) {
+    return robot.getResource(DiscoveryClient.getResourceName(name));
+  }
+}
