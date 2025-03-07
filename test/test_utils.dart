f44c3501212aa132302b358b47d51b6e3d89485e
Ethan Rodkin <ethan@viam.com>
2025-03-07 15:33:15 -0500

    try different take

--- test/test_utils.dart
+++ test/test_utils.dart
@@ -1,13 +1,26 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 
-Future<int> getUnusedPort() {
-  return ServerSocket.bind(InternetAddress.loopbackIPv4, 0).then((socket) {
-    final port = socket.port;
-    socket.close();
-    return port;
+Future<int> getUnusedPort(FutureOr<int> Function(int port) tryPort) async {
+  int? value;
+  await Future.doWhile(() async {
+    value = await tryPort(await getUnsafeUnusedPort());
+    return value == null;
   });
+
+  // We need to set `value` to _something_ to appease typechecker, but in truth we'll never
+  // get here until `value` is non-null
+  return value ?? 54321;
+}
+
+Future<int> getUnsafeUnusedPort() async {
+  late int port;
+  final socket = await RawServerSocket.bind(InternetAddress.loopbackIPv4, 0);
+  port = socket.port;
+  await socket.close();
+  return port;
 }
 
 class TestableWidget extends StatelessWidget {
