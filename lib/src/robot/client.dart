ea60b516c7960662056b70d17fb9f80dd2acb7a8
Ethan <ethan@viam.com>
2025-03-10 13:22:14 -0400

    RSDK-6157 - add initial connection options (#347)
    add initial connection options

--- lib/src/robot/client.dart
+++ lib/src/robot/client.dart
@@ -126,7 +126,7 @@ class RobotClient {
     final client = RobotClient._();
     client._address = url;
     client._options = options;
-    client._channel = await dial(url, options.dialOptions, () => client._sessionsClient.metadata());
+    client._channel = await dialInitial(url, options.dialOptions, () => client._sessionsClient.metadata());
     client._sessionsClient = SessionsClient(client._channel, options.enableSessions, url);
     client._sessionsClient.start();
     client._client = rpb.RobotServiceClient(client._channel);
