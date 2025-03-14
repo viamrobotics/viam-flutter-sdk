504c0572056a7b29b0bc7066ab223e9503bbc509
JosephBorodach <josephborodach@gmail.com>
2025-03-14 13:35:42 -0400

    Run make format

--- test/unit_test/app/app_client_test.dart
+++ test/unit_test/app/app_client_test.dart
@@ -756,8 +756,7 @@ void main() {
     });
 
     test('getOrganizationMetadata', () async {
-      final expected = GetOrganizationMetadataResponse()
-        ..data = (Struct()..fields['key'] = (Value()..stringValue = 'value'));
+      final expected = GetOrganizationMetadataResponse()..data = (Struct()..fields['key'] = (Value()..stringValue = 'value'));
       when(serviceClient.getOrganizationMetadata(any)).thenAnswer((_) => MockResponseFuture.value(expected));
       final response = await appClient.getOrganizationMetadata('orgId');
       expect(response, equals(expected));
