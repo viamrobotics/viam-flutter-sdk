a26aa531fa6e7b0e2e951b3bf6e29821401b36ea
Naveed Jooma <naveed@viam.com>
2025-05-22 16:04:46 -0400

    Add base methods (#391)

--- lib/src/services/generic.dart
+++ lib/src/services/generic.dart
@@ -4,6 +4,7 @@ import '../../protos/common/common.dart';
 import '../gen/common/v1/common.pb.dart';
 import '../gen/service/generic/v1/generic.pbgrpc.dart' as generic_pb;
 import '../resource/base.dart';
+import '../robot/client.dart';
 import '../utils.dart';
 
 /// {@category Services}
@@ -28,5 +29,15 @@ class GenericServiceClient extends Resource with RPCDebugLoggerMixin implements 
       ..command = command.toStruct();
     final response = await client.doCommand(request, options: callOptions);
     return response.result.toMap();
+  }
+
+  /// Get the [ResourceName] for this [GenericServiceClient] with the given [name]
+  static ResourceName getResourceName(String name) {
+    return GenericServiceClient.subtype.getResourceName(name);
+  }
+
+  /// Get the [GenericServiceClient] named [name] from the provided robot.
+  static GenericServiceClient fromRobot(RobotClient robot, String name) {
+    return robot.getResource(GenericServiceClient.getResourceName(name));
   }
 }
