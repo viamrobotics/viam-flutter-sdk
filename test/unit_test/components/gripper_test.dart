af1ee0b16cb57c12750c1543cf203520dccc6546
Gabe Gottlob <gabe.gottlob@viam.com>
2025-06-06 14:34:04 -0400

    add gripper getKinematics

--- test/unit_test/components/gripper_test.dart
+++ test/unit_test/components/gripper_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:grpc/grpc.dart';
 import 'package:viam_sdk/src/components/gripper/service.dart';
+import 'package:viam_sdk/src/gen/common/v1/common.pb.dart';
 import 'package:viam_sdk/src/gen/component/gripper/v1/gripper.pbgrpc.dart';
 import 'package:viam_sdk/src/resource/manager.dart';
 import 'package:viam_sdk/src/utils.dart';
@@ -47,6 +48,12 @@ class FakeGripper extends Gripper {
     isStopped = false;
     this.extra = extra;
   }
+
+  @override
+  Future<Kinematics> getKinematics({Map<String, dynamic>? extra}) async {
+    this.extra = extra;
+    return Kinematics(KinematicsFileFormat.KINEMATICS_FILE_FORMAT_SVA, [1, 2, 3]);
+  }
 }
 
 void main() {
@@ -89,6 +96,12 @@ void main() {
 
       await gripper.stop();
       expect(await gripper.isMoving(), false);
+    });
+
+    test('getKinematics', () async {
+      final kd = await gripper.getKinematics();
+      expect(kd.format, KinematicsFileFormat.KINEMATICS_FILE_FORMAT_SVA);
+      expect(kd.raw, [1, 2, 3]);
     });
 
     test('doCommand', () async {
@@ -169,6 +182,13 @@ void main() {
         expect(resp.isMoving, false);
       });
 
+      test('getKinematics', () async {
+        final client = GripperServiceClient(channel);
+        final resp = await client.getKinematics(GetKinematicsRequest()..name = name);
+        expect(resp.format, KinematicsFileFormat.KINEMATICS_FILE_FORMAT_SVA);
+        expect(resp.kinematicsData, [1, 2, 3]);
+      });
+
       test('doCommand', () async {
         final cmd = {'foo': 'bar'};
 
@@ -225,6 +245,13 @@ void main() {
 
         await gripper.stop();
         expect(await client.isMoving(), false);
+      });
+
+      test('getKinematics', () async {
+        final client = GripperClient(name, channel);
+        final kd = await client.getKinematics();
+        expect(kd.format, KinematicsFileFormat.KINEMATICS_FILE_FORMAT_SVA);
+        expect(kd.raw, [1, 2, 3]);
       });
 
       test('doCommand', () async {
