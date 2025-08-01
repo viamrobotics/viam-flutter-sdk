0309cdbdd70b9d1578a1bde08519c86e73e29a6d
Ethan Rodkin <ethan@viam.com>
2025-08-01 13:29:42 -0400

    stop truncating mdns uri

--- lib/src/rpc/dial.dart
+++ lib/src/rpc/dial.dart
@@ -219,8 +219,7 @@ Future<String> _searchMdns(String address) async {
     } else if (event.type == BonsoirDiscoveryEventType.discoveryServiceResolved) {
       final service = event.service! as ResolvedBonsoirService;
       if (service.name == targetName && service.host != null) {
-        final host = service.host!.substring(0, service.host!.length - 1);
-        localAddress = '$host:${service.port}';
+        localAddress = '${service.host!}:${service.port}';
       }
     }
   });
