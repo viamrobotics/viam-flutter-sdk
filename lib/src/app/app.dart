51c650e8e777e3e64c826a455054108ad07a2b1c
Clint Purser <clint.purser@viam.com>
2025-09-17 17:02:17 -0400

    Improve Get Logs
    I’m updating the Logs screen in the viam mobile app and noticed we’re missing many useful fields in our wrapper. exposing these fields, specifically start/end greatly improve the API call speed from taking ~45 seconds to ~.3 seconds.
    
    I’ve also added an assert and test asserting endTime is after startTime. We do not enforce that on the backend so it can be confusing.
    
    I’ve also updated the unit tests to include the new fields.
    
    Also... this method does follow the cascading like the rest of the SDK now. I found using the cascade notation here with optional nullable fields introduced a lot of complexity needing to check if fields were null before setting them, because the setters from the generated proto code don’t accept nullable types.
    
    I’m open to changing it to be like the rest of the SDK, but IMO this is more readable, and that would introduce a lot of one line if statements after creating `GetRobotPartLogsRequest()`.
    
    Thanks!

--- lib/src/app/app.dart
+++ lib/src/app/app.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:fixnum/fixnum.dart';
+
 import '../../protos/app/packages.dart';
 import '../gen/app/v1/app.pbgrpc.dart';
 import '../gen/common/v1/common.pb.dart';
@@ -331,12 +333,41 @@ class AppClient {
 
   /// Get a page of [LogEntry] for a specific [RobotPart]. Logs are sorted by descending time (newest first)
   ///
+  /// [filter] (string) (optional): Optional string to filter logs on.
+  /// [pageToken] (string) (optional): Optional string indicating which page of logs to query. Defaults to the most recent.
+  /// [levels] (string) (optional): Optional array of log levels to return. Defaults to returning all log levels.
+  /// [limit] (string) (optional): Number of log entries to return. Passing 0 returns all logs. Defaults to 100. All logs or the first
+  /// [startTime] (DateTime) (Optional): The time of the earliest logs to return. Should be earlier than [endTime].
+  /// [endTime] (DateTime) (Optional): The time of the latest logs to return. Should be later than [startTime].
+  ///
   /// For more information, see [Fleet Management API](https://docs.viam.com/appendix/apis/fleet/).
-  Future<RobotPartLogPage> getLogs(String partId, {String? filter, String? pageToken}) async {
-    final request = GetRobotPartLogsRequest()
-      ..id = partId
-      ..filter = filter ?? '';
-    if (pageToken != null) request.pageToken = pageToken;
+  Future<RobotPartLogPage> getLogs(
+    String partId, {
+    String? filter,
+    String? pageToken,
+    Iterable<String>? levels,
+    int? limit,
+    DateTime? startTime,
+    DateTime? endTime,
+    String? source,
+  }) async {
+    final convertedLimit = (limit != null) ? Int64(limit) : null;
+    final start = (startTime != null) ? Timestamp.fromDateTime(startTime) : null;
+    final end = (endTime != null) ? Timestamp.fromDateTime(endTime) : null;
+
+    if (startTime != null && endTime != null) assert(endTime.isAfter(startTime), 'endTime must be later than startTime');
+
+    final request = GetRobotPartLogsRequest(
+      id: partId,
+      filter: filter,
+      pageToken: pageToken,
+      levels: levels,
+      start: start,
+      end: end,
+      limit: convertedLimit,
+      source: source,
+    );
+
     return await _client.getRobotPartLogs(request);
   }
 
