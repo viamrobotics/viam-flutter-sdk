cbad31cb5cd6f4787a5aaf552d1454541f48bb54
Julie Krasnick <julie.krasnick@viam.com>
2025-10-31 14:07:02 -0400

    add in hotspot prov example app

---
+++ example/hotspot_provisioning_example_app/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <bonsoir_windows/bonsoir_windows_plugin_c_api.h>
+#include <flutter_webrtc/flutter_web_r_t_c_plugin.h>
+#include <permission_handler_windows/permission_handler_windows_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  BonsoirWindowsPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("BonsoirWindowsPluginCApi"));
+  FlutterWebRTCPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterWebRTCPlugin"));
+  PermissionHandlerWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
+}
