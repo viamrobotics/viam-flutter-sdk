66cb3d671cb8cfb41f8f8f2d261c7054f1c9d67c
martha-johnston <106617924+martha-johnston@users.noreply.github.com>
2026-01-02 12:35:34 -0500

    [APP-14734] Add input controller to flutter sdk (#470)
    * add input controller to flutter sdk
    
    * remove getGeometries, fix callback logic

---
+++ lib/src/components/input_controller/client.dart
@@ -0,0 +1,213 @@
+import 'dart:async';
+
+import 'package:grpc/grpc_connection_interface.dart';
+
+import '../../gen/common/v1/common.pb.dart' as common_pb;
+import '../../gen/component/inputcontroller/v1/input_controller.pbgrpc.dart';
+import '../../gen/google/protobuf/struct.pb.dart';
+import '../../gen/google/protobuf/timestamp.pb.dart';
+import '../../resource/base.dart';
+import '../../utils.dart';
+import 'input_controller.dart';
+
+/// {@category Components}
+/// gRPC client for the [InputController] component.
+class InputControllerClient extends InputController with RPCDebugLoggerMixin implements ResourceRPCClient {
+  @override
+  final String name;
+
+  @override
+  ClientChannelBase channel;
+
+  @override
+  InputControllerServiceClient get client => InputControllerServiceClient(channel);
+
+  final Map<String, Map<String, void Function(Event)?>> _callbacks = {};
+
+  StreamSubscription<StreamEventsResponse>? _streamSubscription;
+  bool _streamRunning = false;
+  bool _streamNeedsReconnect = false;
+  Struct? _extra;
+
+  InputControllerClient(this.name, this.channel);
+
+  @override
+  Future<List<String>> getControls({Map<String, dynamic>? extra}) async {
+    final request = GetControlsRequest()
+      ..controller = name
+      ..extra = extra?.toStruct() ?? Struct();
+    final response = await client.getControls(request, options: callOptions);
+    return response.controls;
+  }
+
+  @override
+  Future<List<Event>> getEvents({Map<String, dynamic>? extra}) async {
+    final request = GetEventsRequest()
+      ..controller = name
+      ..extra = extra?.toStruct() ?? Struct();
+    final response = await client.getEvents(request, options: callOptions);
+    return response.events;
+  }
+
+  @override
+  Future<void> triggerEvent(Event event, {Map<String, dynamic>? extra}) async {
+    final request = TriggerEventRequest()
+      ..controller = name
+      ..event = event
+      ..extra = extra?.toStruct() ?? Struct();
+    await client.triggerEvent(request, options: callOptions);
+  }
+
+  Future<common_pb.GetGeometriesResponse> getGeometries({Map<String, dynamic>? extra}) async {
+    final request = common_pb.GetGeometriesRequest()
+      ..name = name
+      ..extra = extra?.toStruct() ?? Struct();
+    return await client.getGeometries(request, options: callOptions);
+  }
+
+  @override
+  Future<void> registerControlCallback(String control, List<String> triggers, void Function(Event)? callback,
+      {Map<String, dynamic>? extra}) async {
+    if (extra != null) {
+      _extra = extra.toStruct();
+    }
+
+    if (!_callbacks.containsKey(control)) {
+      _callbacks[control] = {};
+    }
+
+    for (final trigger in triggers) {
+      if (trigger == 'ButtonChange') {
+        _callbacks[control]!['ButtonPress'] = callback;
+        _callbacks[control]!['ButtonRelease'] = callback;
+      } else {
+        _callbacks[control]![trigger] = callback;
+      }
+    }
+
+    if (_streamRunning) {
+      _streamNeedsReconnect = true;
+      await _streamSubscription?.cancel();
+      _streamSubscription = null;
+    } else {
+      _streamRunning = true;
+      _connectStream();
+    }
+  }
+
+  void _connectStream() {
+    Future(() async {
+      while (true) {
+        bool haveCallbacks = false;
+        final request = StreamEventsRequest()
+          ..controller = name
+          ..extra = _extra ?? Struct();
+
+        for (final entry in _callbacks.entries) {
+          final controlEvents = StreamEventsRequest_Events()..control = entry.key;
+
+          for (final eventEntry in entry.value.entries) {
+            if (eventEntry.value != null) {
+              haveCallbacks = true;
+              controlEvents.events.add(eventEntry.key);
+            } else {
+              controlEvents.cancelledEvents.add(eventEntry.key);
+            }
+          }
+
+          if (controlEvents.events.isNotEmpty) {
+            request.events.add(controlEvents);
+          } else if (controlEvents.cancelledEvents.isNotEmpty) {
+            // Even if only cancelled events, we need to send the request to update the server
+            request.events.add(controlEvents);
+          }
+        }
+
+        if (!haveCallbacks) {
+          _streamRunning = false;
+          _streamNeedsReconnect = false;
+          return;
+        }
+
+        _streamNeedsReconnect = false;
+
+        // Create one stream
+        try {
+          final stream = client.streamEvents(request, options: callOptions);
+
+          _sendConnectionStatus(true);
+
+          // Handle stream events
+          await for (final response in stream) {
+            if (_streamNeedsReconnect) {
+              break;
+            }
+
+            if (response.hasEvent()) {
+              _execCallback(response.event);
+            }
+          }
+
+          if (_streamNeedsReconnect) {
+            continue;
+          }
+
+          _sendConnectionStatus(false);
+
+          await Future.delayed(const Duration(seconds: 3));
+        } catch (error) {
+          if (_streamNeedsReconnect) {
+            continue;
+          }
+
+          _sendConnectionStatus(false);
+
+          await Future.delayed(const Duration(seconds: 3));
+        } finally {
+          _streamSubscription = null;
+        }
+      }
+    });
+  }
+
+  void _sendConnectionStatus(bool connected) {
+    final eventType = connected ? 'Connect' : 'Disconnect';
+    final timestamp = Timestamp.fromDateTime(DateTime.now());
+
+    for (final control in _callbacks.keys) {
+      final event = Event()
+        ..time = timestamp
+        ..event = eventType
+        ..control = control
+        ..value = 0.0;
+
+      _execCallback(event);
+    }
+  }
+
+  void _execCallback(Event event) {
+    final controlCallbacks = _callbacks[event.control];
+    if (controlCallbacks == null) {
+      return;
+    }
+
+    final callback = controlCallbacks[event.event];
+    if (callback != null) {
+      Future(() => callback(event));
+    }
+
+    final allEventsCallback = controlCallbacks['AllEvents'];
+    if (allEventsCallback != null) {
+      Future(() => allEventsCallback(event));
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> doCommand(Map<String, dynamic> command) async {
+    final request = common_pb.DoCommandRequest()
+      ..name = name
+      ..command = command.toStruct();
+    final response = await client.doCommand(request, options: callOptions);
+    return response.result.toMap();
+  }
+}
---
+++ lib/src/components/input_controller/input_controller.dart
@@ -0,0 +1,86 @@
+import '../../gen/common/v1/common.pb.dart';
+import '../../gen/component/inputcontroller/v1/input_controller.pb.dart';
+import '../../resource/base.dart';
+import '../../robot/client.dart';
+
+/// {@category Components}
+/// InputController represents a physical input controller.
+///
+/// For more information, see [InputController component](https://docs.viam.com/dev/reference/apis/components/input-controller/).
+abstract class InputController extends Resource {
+  static const Subtype subtype = Subtype(resourceNamespaceRDK, resourceTypeComponent, 'input_controller');
+
+  /// Get the list of available controls on the input controller.
+  ///
+  /// ```
+  /// final controls = await myInputController.getControls();
+  /// ```
+  ///
+  /// For more information, see [InputController component](https://docs.viam.com/dev/reference/apis/components/input-controller/#getcontrols).
+  Future<List<String>> getControls({Map<String, dynamic>? extra});
+
+  /// Get events for each control on the input controller.
+  ///
+  /// ```
+  /// final events = await myInputController.getEvents();
+  /// ```
+  ///
+  /// For more information, see [InputController component](https://docs.viam.com/dev/reference/apis/components/input-controller/#getevents).
+  Future<List<Event>> getEvents({Map<String, dynamic>? extra});
+
+  /// Trigger an event on the input controller.
+  ///
+  /// ```
+  /// final event = Event()
+  ///   ..control = 'Button1'
+  ///   ..event = 'ButtonPress'
+  ///   ..value = 1.0;
+  /// await myInputController.triggerEvent(event);
+  /// ```
+  ///
+  /// For more information, see [InputController component](https://docs.viam.com/dev/reference/apis/components/input-controller/#triggerevent).
+  Future<void> triggerEvent(Event event, {Map<String, dynamic>? extra});
+
+  /// Register a callback function to be called when control events occur.
+  ///
+  /// [control] is the name of the control (e.g., "ButtonSouth", "AbsoluteX").
+  /// [triggers] is a list of event types to listen for (e.g., ["ButtonPress", "ButtonRelease"]).
+  /// If "ButtonChange" is included, it will register callbacks for both "ButtonPress" and "ButtonRelease".
+  /// [callback] is the function to call when matching events occur. If null, the callbacks for those triggers will be unregistered.
+  ///
+  /// ```
+  /// await myInputController.registerControlCallback(
+  ///   "ButtonSouth",
+  ///   ["ButtonPress", "ButtonRelease"],
+  ///   (Event event) {
+  ///     print('Control event: ${event.control}, value: ${event.value}');
+  ///   },
+  /// );
+  /// ```
+  ///
+  /// For more information, see [InputController component](https://docs.viam.com/dev/reference/apis/components/input-controller/).
+  Future<void> registerControlCallback(String control, List<String> triggers, void Function(Event)? callback,
+      {Map<String, dynamic>? extra});
+
+  /// Get the [ResourceName] for this [InputController] with the given [name]
+  ///
+  /// ```
+  /// final myInputControllerResourceName = myInputController.getResourceName("my_inputController");
+  /// ```
+  ///
+  /// For more information, see [InputController component](https://docs.viam.com/dev/reference/apis/components/input-controller/#getresourcename).
+  static ResourceName getResourceName(String name) {
+    return InputController.subtype.getResourceName(name);
+  }
+
+  /// Get the [InputController] named [name] from the provided robot.
+  ///
+  /// ```
+  /// final myInputController = InputController.fromRobot(myRobotClient, "my_inputController");
+  /// ```
+  ///
+  /// For more information, see [InputController component](https://docs.viam.com/dev/reference/apis/components/input-controller/).
+  static InputController fromRobot(RobotClient robot, String name) {
+    return robot.getResource(InputController.getResourceName(name));
+  }
+}
