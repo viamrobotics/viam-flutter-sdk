c21f4368f0541f69d80bf663e3c2cff12a4b8b10
Kevin Johnson <kevin.johnson@viam.com>
2026-02-23 16:03:35 -0500

    Update client.dart

--- lib/src/robot/client.dart
+++ lib/src/robot/client.dart
@@ -96,8 +96,8 @@ class RobotClient {
     final client = RobotClient._();
     client._address = url;
     client._options = options;
-    client._channel = await dialInitial(url, options.dialOptions, () => client._sessionsClient.metadata());
     client._sessionsClient = SessionsClient(client._channel, options.enableSessions, url);
+    client._channel = await dialInitial(url, options.dialOptions, () => client._sessionsClient.metadata());
     client._sessionsClient.start();
     client._client = rpb.RobotServiceClient(client._channel);
     client._streamManager = StreamManager(client._channel as WebRtcClientChannel);
